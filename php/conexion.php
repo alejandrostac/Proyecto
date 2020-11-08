fe2a987a9ddd95b57de09a471c6fe871dc3bd38a
Alejandro <judaistrife21@gmail.com>
2020-11-08 14:57:57 -0600

    combinacion de trabajos

--- php/conexion.php
+++ php/conexion.php
@@ -2,6 +2,6 @@
 	$host = "127.0.0.1";
 	$data_user = "root";
 	$password = "";
-	$database = "control_de _usuarios";
+	$database = "proyecto";
 	$enlace = mysqli_connect($host, $data_user, $password, $database);
 ?>
