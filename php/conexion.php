f056334ed277fbd4eb8e4ba83126c5fa3c56339b
Lscamas92 <73980313+Lscamas92@users.noreply.github.com>
2020-11-14 16:29:02 -0600

    cambios

--- php/conexion.php
+++ php/conexion.php
@@ -2,6 +2,6 @@
 	$host = "127.0.0.1";
 	$data_user = "root";
 	$password = "";
-	$database = "proyecto";
+	$database = "control_de _usuarios";
 	$enlace = mysqli_connect($host, $data_user, $password, $database);
 ?>
