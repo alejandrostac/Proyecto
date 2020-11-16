3d2bdbaa7f14fe37078cebcae0a8d34f0b3d1a65
Alejandro <judaistrife21@gmail.com>
2020-11-15 18:54:25 -0600

    se le dio funcion a los examenes

--- php/rate.php
+++ php/rate.php
@@ -1,3 +1,17 @@
 <?php
-	echo "Examen contestado";
+	require('conexion.php');
+
+	date_default_timezone_set('America/Mexico_City');
+	$date = date("d-m-y");
+	$time = date("H:i:s");
+	$name = $_POST['name'];
+	$fine = $_POST['q1']+$_POST['q2']+$_POST['q3']+$_POST['q4']+$_POST['q5']+$_POST['q6']+$_POST['q7']+$_POST['q8']+$_POST['q9']+$_POST['q10'];
+	$fail = 10 - $fine;
+
+	$query = "INSERT INTO tests (name,date,time,correct,incorrect,id_student) VALUES ('".$name."','".$date."','".$time."','".$fine."','".$fail."','1')";
+	if(mysqli_query($enlace, $query)){
+		header("location: ../student.html");
+	}else{
+		echo "no se pudo conectar a la base de datos";
+	}
 ?>
